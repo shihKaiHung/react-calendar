d277d3569b31516fa54dab7ce7e70441442c2793
shikai.hung <shikai.hung@rdgroup.com.tw>
2018-11-13 15:38:47 +0800

    Finsh Days

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -30,22 +30,46 @@ export class CalendarDay extends React.Component<{}, ClendarDayState> {
     });
   }
 
+  renderDays() {
+    const start = this.state.year.clone().startOf('month').weekday(0);
+    const end = this.state.year.clone().endOf('month').weekday(6);
+    const days = end.diff(start, 'days');
+    const daysArray = [];
+    const now = moment().startOf('day');
+    for (let i = 0 ; i < days + 1; i++) {
+      const currentDay = start;
+      daysArray.push({
+        label: currentDay.format('D'),
+        currentMonth: currentDay.month() === this.state.year.month() && currentDay.year() === this.state.year.year(),
+        isToday: currentDay.isSame(now),
+      });
+      start.add(1, "days");
+    }
+    return (
+        daysArray.map((d, index) =>
+        <Days currentMonth={d.currentMonth} isToday={d.isToday} key={index}>
+          {d.label}
+        </Days>)
+    );
+  }
+
   public render() {
-    const start = moment().clone().startOf('month').weekday(0);
-    const end = moment().clone().endOf('month').weekday(6);
-    const days = end.diff(start, 'days')
-    console.log(start.format('dddd DD'))
-    console.log(end.format('DD'))
-    console.log(days)
     return (
       <Wrap>
         <CalendarSelect>
           <i className="fa fa-angle-left" aria-hidden="true" onClick={this.handleSubtractMonth} />
-          <CalendarDate>{this.state.year.format('MMMM YYYY' )}</CalendarDate>
+          <CalendarDate>{this.state.year.format('MMMM YYYY')}</CalendarDate>
           <i className="fa fa-angle-right" aria-hidden="true" onClick={this.handleAddMonth} />
         </CalendarSelect>
         <WeekWrap>
-          {day.map(i => <WeekTitle>{dayName[i]}</WeekTitle>)}
+          {day.map((i, index) =>
+            <WeekTitle key={index}>
+              {dayName[i]}
+            </WeekTitle>)
+          }
+        </WeekWrap>
+        <WeekWrap>
+          {this.renderDays()}
         </WeekWrap>
       </Wrap>
     );
@@ -63,6 +87,11 @@ const CalendarDate = styled.div`
   text-align: center;
   font-size: 12px;
   color: #4a4a4a;
+  cursor: pointer;
+  margin: 0 20px;
+  &:hover{
+    background-color: #f2f2f2;
+  }
 `;
 
 const CalendarSelect = styled.div`
@@ -70,10 +99,14 @@ const CalendarSelect = styled.div`
   box-sizing: border-box;
   width: 100%;
   display: flex;
+  i {
+    cursor: pointer;
+  }
 `;
 
 const WeekWrap = styled.div`
   display: flex;
+  flex-flow: wrap;
   justify-content: center;
   box-sizing: border-box;
   padding: 0 15px;
@@ -83,5 +116,16 @@ const WeekTitle = styled.p`
   margin: 0;
   font-size: 12px;
   flex: 1 1 auto;
-  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Days = styled(WeekTitle)`
+  flex: 22px;
+  width: 22px;
+  height: 22px;
+  border-radius: 15px;
+  background-color: ${props => props.isToday ? "#ff5151" : "none"};
+  color: ${props => props.currentMonth ? props.isToday ? "#fff" : "#4a4a4a" : "#d0d0d0"}
 `;
